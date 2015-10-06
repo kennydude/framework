3da06c7084e386a1d354c84c889a35c1f0a78ff7
rivets <nlfm@tomat.ncl.ac.uk>
2015-10-06 11:52:00 +0100

    Add editting method

--- class/models/model_user.php
+++ class/models/model_user.php
@@ -1,20 +1,18 @@
 <?php
 /**
- * Contains the definition of the Model class for User beans
+ * A model class for the RedBean object User
  *
  * @author Lindsay Marshall <lindsay.marshall@ncl.ac.uk>
- * @copyright 2013-2015 Newcastle University
- */
-/**
- * A model class for the RedBean object User
+ * @copyright 2013-2014 Newcastle University
+ *
  */
     class Model_User extends RedBean_SimpleModel
     {
 /**
  * Check for a role
  *
- * @param string	$contextname
- * @param string	$rolename
+ * @param string        $contextname    The name of a context...
+ * @param string	$rolename       The name of a role....
  *
  * @return object
  */
@@ -28,8 +26,8 @@
 /**
  * Check for a role
  *
- * @param string	$contextname
- * @param string	$rolename
+ * @param string	$contextname    The name of a context...
+ * @param string	$rolename       The name of a role....
  *
  * @return void
  */
@@ -47,9 +45,9 @@
 /**
  *  Add a role
  *
- * @param string	$contextname
- * @param string	$rolename
- * @param string	$otherinfo
+ * @param string	$contextname    The name of a context...
+ * @param string	$rolename       The name of a role....
+ * @param string	$otherinfo      Any other info that is to be stored with the role
  * @param string	$start		A datetime
  * @param string	$end		A datetime or ''
  *
@@ -58,7 +56,15 @@
         public function addrole($contextname, $rolename, $otherinfo, $start, $end = '')
         {
             $cname = R::findOne('rolecontext', 'name=?', array($contextname));
+            if (!is_object($cname))
+            {
+                (new Web)->bad();
+            }
             $rname = R::findOne('rolename', 'name=?', array($rolename));
+            if (!is_object($rname))
+            {
+                (new Web)->bad();
+            }
             $r = R::dispense('role');
             $r->user = $this->bean;
             $r->rolecontext = $cname;
@@ -71,11 +77,17 @@
 /**
  * Get all currently valid roles for this user
  *
+ * @param boolean	$all	If TRUE then include expired roles
+ *
  * @return array
  */
-        public function roles()
+        public function roles($all = FALSE)
         {
-            return $this->bean->withCondition('start <= NOW() and (end is null or end >= NOW())')->ownRole;
+	    if ($all)
+	    {
+	        return $this->bean->with('order by start,end')->ownRole;
+	    }
+            return $this->bean->withCondition('start <= NOW() and (end is null or end >= NOW()) order by start, end')->ownRole;
         }
 /**
  * Is this user an admin?
@@ -150,8 +162,6 @@
 /**
  * Generate a token for this user that can be used as a unique id from a phone.
  *
- * @param string        $device     Currently not used....
- *
  * @return string
  */
 	public function maketoken($device = '')
@@ -162,5 +172,82 @@
 	    $token->sub = $this->bean->getID();
 	    return JWT::encode($token, Context::KEY);
 	}
+/**
+ * Handle an edit form for this user
+ *
+ * @return void
+ */
+        public function edit($context)
+        {
+            $change = FALSE;
+            foreach (array('email') as $fld)
+            { // might eneed more fields for different applications
+                $val = $context->postpar($fld, '');
+                if ($fld == '')
+                { // this is an error as these must be present
+                }
+                elseif ($val != $this->bean->$fld)
+                {
+                    $this->bean->$fld = $val;
+                    $change = TRUE;
+                }
+            }
+            $pw = $context->postpar('pw', '');
+            if ($pw != '' && $pw == $context->postpar('rpw', ''))
+            {
+                $this->setpw($pw);
+                $change = FALSE; // setting the password will do a store...
+            }
+            if ($change)
+            {
+                R::store($this->bean);
+            }
+            $uroles = $this->roles();
+            foreach ($_POST['exist'] as $ix => $rid)
+            {
+                $rl = $context->load('role', $rid);
+                $start = $_POST['xstart'][$ix];
+                $end = $_POST['xend'][$ix];
+                $other = $_POST['xotherinfo'][$ix];
+                if (strtolower($start) == 'now')
+                {
+                    $rl->start = $context->utcnow();
+                }
+                elseif ($start != $rl->start)
+                {
+                    $rl->start = $context->utcdate($start);
+                }
+                if (strtolower($end) == 'never' || $end == '')
+                {
+                    if ($rl->end != '')
+                    {
+                        $rl->end = NULL;
+                    }
+                }
+                elseif ($end != $rl->end)
+                {
+                     $rl->end = $context->utcdate($end);
+                }
+                if ($other != $rl->otherinfo)
+                {
+                    $rl->otherinfo = $other;
+                }
+                R::store($rl);
+            }
+            foreach ($_POST['role'] as $ix => $rn)
+            {
+                $cn = $_POST['context'][$ix];
+                if ($rn != '' && $cn != '')
+                {
+                    $end = $_POST['end'][$ix];
+                    $start = $_POST['start'][$ix];
+                    $this->addrole($cn, $rn, $_POST['otherinfo'][$ix],
+                        strtolower($start) == 'now' ? $context->utcnow() : $context->utcdate($start),
+                        strtolower($end) == 'never' || $end == '' ? '' : $context->utcdate($end)
+                    );
+                }
+            }
+            return TRUE;
+        }
     }
 ?>
