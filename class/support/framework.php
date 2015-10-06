c7d3920fc2db93b9de31b80d6a5d515a58208655
rivets <nlfm@tomat.ncl.ac.uk>
2015-10-06 11:53:47 +0100

    Include vendor autoload so as to be prpared for Twig 2.0

--- class/support/framework.php
+++ class/support/framework.php
@@ -1,19 +1,8 @@
 <?php
 /**
- * Contains definition of Framework class
- *
- * @author Lindsay Marshall <lindsay.marshall@ncl.ac.uk>
- * @copyright 2015 Newcastle University
- */
-/**
  * Class for doing initial setup of the Framework.
- */
-    class Framework
-    {
-/**
- * Carry out some global initialisations
  *
- * This is called in index.php and ajax.php. It does mean that it thefile to be included directly as
+ * This is loaded in index.php and ajax.php. It does mean that it has to be included as
  * it is setting up the autoloader and stuff. But it does keep things DRY - only one place to
  * add any new autoload places etc.
  *
@@ -21,6 +10,12 @@
  * it seems to make more sense than having to create an instance that is never going to
  * be used again. (Equally this could just be code in a file rather than being a function
  * or a class, but that just seems nasty)
+ *
+ */
+    class Framework
+    {
+/**
+ * Initialise some standard things for any invocation of a page
  *
  * @return void
  */
@@ -33,15 +28,17 @@
             $dir = dirname(dirname(__DIR__));
             set_include_path(
                 implode(PATH_SEPARATOR, array(
-                    implode(DIRECTORY_SEPARATOR, array($dir, '/class')),
-                    implode(DIRECTORY_SEPARATOR, array($dir, '/class/support')),
-                    implode(DIRECTORY_SEPARATOR, array($dir, '/class/models')),
-                    implode(DIRECTORY_SEPARATOR, array($dir, '/lib')),
+                    implode(DIRECTORY_SEPARATOR, array($dir, 'class')),
+                    implode(DIRECTORY_SEPARATOR, array($dir, 'class/support')),
+                    implode(DIRECTORY_SEPARATOR, array($dir, 'class/models')),
+                    implode(DIRECTORY_SEPARATOR, array($dir, 'lib')),
                     get_include_path()
                 ))
             );
             spl_autoload_extensions('.php');
             spl_autoload_register();
+
+            include $dir.'/vendor/autoload.php';
         }
     }
 ?>
