d5fbf26ef0b21f1dc912899b5638b8007a4802a6
rivets <nlfm@tomat.ncl.ac.uk>
2015-10-06 11:52:27 +0100

    Add REST edit operation

--- class/support/admin.php
+++ class/support/admin.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Contaisn definition of Admin class
+ * Contains definition of Admin class
  *
  * @author Lindsay Marshall <lindsay.marshall@ncl.ac.uk>
  * @copyright 2012-2015 Newcastle University
@@ -16,11 +16,10 @@
  * Handle various admin operations /admin/xxxx
  *
  * @param object	$context	The context object for the site
- * @param object	$local		The local object for the site
  *
  * @return string	A template name
  */
-	public function handle($context, $local)
+	public function handle($context)
 	{
 	    $tpl = 'support/admin.twig';
 	    $rest = $context->rest();
@@ -46,6 +45,30 @@
 		$_SERVER['PHP_AUTH_PW'] = '*************'; # hide the password in case it is showing.
 	        phpinfo();
 		exit;
+
+	    case 'edit' : // Edit something - at the moment just a User
+	        if (count($rest) < 3)
+		{
+		    (new Web)->bad();
+		}
+	        $kind = $rest[1];
+                $obj = $context->load($kind, $rest[2]);
+                if (!is_object($obj))
+                {
+                    (new Web)->bad();
+                }
+                if (($bid = $context->postpar('bean', '')) != '')
+                { # this is a post
+                    if ($bid != $obj->getID())
+                    { # something odd...
+                        (new Web)->bad();
+                    }
+                    $obj->edit($context);
+                    // The edit call might divert to somewhere else so sometimes we may not get here.
+                }
+		$context->local()->addval($kind, $obj);
+		$tpl = 'support/edit'.$kind.'.twig';
+		break;
 
 	    default :
 		break;
