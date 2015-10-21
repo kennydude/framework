d6339343d64114b9d54413143e1a6e5624ae1817
rivets <nlfm@tomat.ncl.ac.uk>
2015-10-21 16:19:18 +0100

    Change an == to ===

--- class/contact.php
+++ class/contact.php
@@ -3,7 +3,7 @@
  * A class that contains code to implement a contact page
  *
  * @author Lindsay Marshall <lindsay.marshall@ncl.ac.uk>
- * @copyright 2012-2013 Newcastle University
+ * @copyright 2012-2015 Newcastle University
  *
  */
 /**
@@ -20,7 +20,7 @@
  */
         public function handle($context)
         {
-            if (($msg = $context->postpar('message', '')) != '')
+            if (($msg = $context->postpar('message', '')) !== '')
             { # there is a post
                 mail(Config::SYSADMIN, $context->postpar('subject', 'No Subject'), $msg);
                 $context->local()->addval('done', TRUE);
