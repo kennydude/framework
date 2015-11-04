fe25135c6bd9d26bb60274fc7953f6386f569412
rivets <nlfm@tomat.ncl.ac.uk>
2015-11-04 15:52:45 +0000

    Clean up code from initial creation

--- class/support/faiterator.php
+++ class/support/faiterator.php
@@ -10,13 +10,32 @@
  */
     class FAIterator extends ArrayIterator
     {
+/**
+ * @var array   The 'error' array fron the named file.
+ *
+ * This is just used an aiteration control and any of the field arrays could be used.
+ */
         private $far;
+/**
+ *  The constructor
+ *
+ *  @param string    $name The name of the entry in the $_FILES array
+ */
         public function __construct($name)
         {
-	    $this->far = $_FILES[$name]
+	    $this->far = $_FILES[$name];
 	    parent::_construct($_FILES[$name]['error']);
         }
-
+/**
+ * Returns the value of the current element of the "array"
+ *
+ * This returns an array that looks like what you would get if the file upload was a
+ * singleton and not an array. This can simplify coding in certain circumstances even
+ * though it invovles restructuring the data and hence is slightly inefficient. Basically
+ * the $_FILES array should be made from objects with fields not arrays of keyed values!!!
+ *
+ * @return array
+ */
         public function current()
         {
             $x = $this->far;
@@ -25,8 +44,8 @@
 	        'name'      => $x['name'][$k],
 	        'type'      => $x['type'][$k],
 	        'size'      => $x['size'][$k],
-	        'tmp_name   => $x['tmp_name'][$k],
-	        'error      => $x['error'][$k],
+	        'tmp_name'  => $x['tmp_name'][$k],
+	        'error'     => $x['error'][$k],
 	    );
         }
     }
