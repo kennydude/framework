b79e22755e1a95e799675f715c6d54070542244c
rivets <nlfm@tomat.ncl.ac.uk>
2015-12-10 09:42:28 +0000

    Re format  and Tidy

--- assets/js/bgfade.js
+++ assets/js/bgfade.js
@@ -1,8 +1,6 @@
     function easeInOut(minValue,maxValue,totalSteps,actualStep,powr)
     {
-	var delta = maxValue - minValue;
-	var stepp = minValue+(Math.pow(((1 / totalSteps)*actualStep),powr)*delta);
-	return Math.ceil(stepp)
+	return Math.ceil(minValue+(Math.pow(((1 / totalSteps)*actualStep),powr)*(maxValue - minValue)))
     }
 
     function doBGFade(elem,startRGB,endRGB,finalColor,steps,intervals,powr)
@@ -11,15 +9,16 @@
 	var actStep = 0;
 	elem.bgFadeInt = window.setInterval(
 	    function() {
-		    elem.css("backgroundColor", "rgb("+
-			    easeInOut(startRGB[0],endRGB[0],steps,actStep,powr)+","+
-			    easeInOut(startRGB[1],endRGB[1],steps,actStep,powr)+","+
-			    easeInOut(startRGB[2],endRGB[2],steps,actStep,powr)+")"
-		    );
+		    elem.css('backgroundColor', 'rgb(' +
+			    easeInOut(startRGB[0], endRGB[0], steps, actStep, powr) + ',' +
+			    easeInOut(startRGB[1], endRGB[1], steps, actStep, powr) + ',' +
+			    easeInOut(startRGB[2], endRGB[2], steps, actStep, powr) + ')'
+		    )
 		    actStep++;
-		    if (actStep > steps) {
-		    elem.css("backgroundColor", finalColor);
-		    window.clearInterval(elem.bgFadeInt);
+		    if (actStep > steps)
+                    {
+		        elem.css('backgroundcolor', finalColor)
+		        window.clearInterval(elem.bgFadeInt)
 		    }
 	    }
 	    ,intervals)
