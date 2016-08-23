9c3599a582c5bac2cdaea7ace75a3e6bab421b47
Lindsay Marshall <lindsay.marshall@ncl.ac.uk>
2016-08-23 16:39:18 +0100

    Add in new page types and use new sending functions. Also allow array returns from handle functions so that more compelx resutls can be generated.

--- index.php
+++ index.php
@@ -33,14 +33,22 @@
  *                 /                        =>        /home and then
  *                 /action/r/e/st/          =>        Broken down in Context class. An action and an array of parameters.
  *
- *         Query strings and/or post fields are in the $_ arrays as normal.
+ *         Query strings and/or post fields are in the $_ arrays as normal but please use the access functions provided
+ *         to get at the values whenever appropriate!
  */
     include 'class/support/framework.php';
     Framework::initialise();
+    Config::setup(); # add default headers etc. - anything that the user choses to add to the code.
 
     $local = Local::getinstance()->setup(__DIR__, FALSE, TRUE, TRUE, TRUE); # Not Ajax, debug on, load twig, load RB
     $context = Context::getinstance()->setup();
 
+    if (file_exists($local->makepath($local->basedir(), 'maintenance')) && !$context->hasadmin())
+    { # only let administrators in as we are doing maintenance. Could have a similar feature for
+	$context->web()->sendtemplate('support/maintenance.twig', StatusCodes::HTTP_OK, 'text/html',
+	    ['msg' => file_get_contents($local->makepath($local->basedir(), 'maintenance'))]);
+	exit;
+    }
     $action = $context->action();
     if ($action === '')
     {
@@ -50,49 +58,64 @@
     $page = R::findOne('page', 'name=? and active=?', array($action, 1));
     if (!is_object($page))
     { # No such page or it is marked as inactive
-        $context->divert('/error/404?page='.urlencode($_SERVER['REQUEST_URI']));
-        # does not return
+       $page = new stdClass;
+       $page->kind = Siteaction::OBJECT;
+       $page->source = 'NoPage';
     }
-
-    if (($page->needlogin) && !$context->hasuser())
-    { # not logged in or not an admin
-        $context->divert('/login?page='.urlencode($local->debase($_SERVER['REQUEST_URI'])));
-    }
-
-    if ($page->admin && !$context->hasadmin())
-    { # not logged in or not an admin
-        $context->divert('/error/403');
-    }
-
-    if ($page->devel && !$context->hasdeveloper())
-    { # not logged in or not a developer
-        $context->divert('/error/403');
-    }
-
-    if ($page->mobileonly && !$context->hastoken())
+    else
     {
-	$context->divert('/error/403');
+	if (($page->needlogin) && !$context->hasuser())
+	{ # not logged in or not an admin
+	    $context->divert('/login?page='.urlencode($local->debase($_SERVER['REQUEST_URI'])));
+	}
+    
+	if ($page->admin && !$context->hasadmin())
+	{ # not logged in or not an admin
+	    $context->divert('/error/403');
+	}
+    
+	if ($page->devel && !$context->hasdeveloper())
+	{ # not logged in or not a developer
+	    $context->divert('/error/403');
+	}
+    
+	if ($page->mobileonly && !$context->hastoken())
+	{
+	    $context->divert('/error/403');
+	}
     }
 
     $local->addval('context', $context);
     $local->addval('page', $action);
     $local->addval('siteinfo', new Siteinfo($local));
 
+    $mime = 'text/html; charset=utf-8';
+    $code = StatusCodes::HTTP_OK;
     switch ($page->kind)
     {
     case Siteaction::OBJECT:
         $tpl = (new $page->source)->handle($context);
+	if (is_array($tpl))
+	{
+	    list($tpl, $mime, $code) = $tpl;
+	}
         break;
 
     case Siteaction::TEMPLATE:
         $tpl = $page->source;
         break;
 
+    case Siteaction::REDIRECT:
+        $context->divert($page->source, TRUE);
+        /* NOT REACHED */
+
+    case Siteaction::REHOME:
+        $context->divert($page->source, FALSE);
+        /* NOT REACHED */
+
     default :
         $context->web()->internal('Weird error');
+        /* NOT REACHED */
     }
 
-    ob_start('ob_gzhandler');
-    $local->render($tpl);
-    ob_end_flush();
-?>
+    $context->web()->sendtemplate($tpl, $code, $mime);
