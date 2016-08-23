1c0d2541dec5ffd6a6fe970af1e53f7279a8b42c
Lindsay Marshall <lindsay.marshall@ncl.ac.uk>
2016-08-23 16:33:26 +0100

    Use new PHP literal array syntax

--- class/support/framework.php
+++ class/support/framework.php
@@ -30,13 +30,13 @@
  */
             $dir = dirname(dirname(__DIR__));
             set_include_path(
-                implode(PATH_SEPARATOR, array(
-                    implode(DIRECTORY_SEPARATOR, array($dir, 'class')),
-                    implode(DIRECTORY_SEPARATOR, array($dir, 'class/support')),
-                    implode(DIRECTORY_SEPARATOR, array($dir, 'class/models')),
-                    implode(DIRECTORY_SEPARATOR, array($dir, 'lib')),
+                implode(PATH_SEPARATOR, [
+                    implode(DIRECTORY_SEPARATOR, [$dir, 'class']),
+                    implode(DIRECTORY_SEPARATOR, [$dir, 'class/support']),
+                    implode(DIRECTORY_SEPARATOR, [$dir, 'class/models']),
+                    implode(DIRECTORY_SEPARATOR, [$dir, 'lib']),
                     get_include_path()
-                ))
+                ])
             );
             spl_autoload_extensions('.php');
             spl_autoload_register();
